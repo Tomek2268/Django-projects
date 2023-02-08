8f70b7dc8b5513d7b1e06cd47234f07ec5e7bdbf
Tomek2268 <szczepankiewicz.tomasz22@gmail.com>
2023-02-08 22:13:12 +0100

    ttt local with no reloads

--- games/api/urls.py
+++ games/api/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
 
     path('',views.get_routes),
-    path('tic_tac_toe/<str:pk>/',views.get_tic_tac_toe),
-    path('tic_tac_toe/<str:pk>/update/',views.update_tic_tac_toe),
     path('user_joined/',views.user_joined),
     path('user_left/',views.user_left),
     path('player_move/',views.player_move),
--- games/api/views.py
+++ games/api/views.py
@@ -1,52 +1,19 @@
-from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .pusher import pusher_client
-from .serializers import TicTacToeGameSerializer
 from ..models import TicTacToeGame
 
 @api_view(['GET'])
 def get_routes(request):
 
     routes = [
-        {'GET':'/api/tic_tac_toe/id'},
-        {'PUT':'/api/tic_tac_toe/id/update'},
 
         {'POST':'/api/users/token'},
         {'POST':'/api/users/token/refresh'}
     ]
 
     return Response(routes)
-
-@api_view(['GET'])
-def get_tic_tac_toe(request,pk):
-    game = TicTacToeGame.objects.get(id=pk)
-    serializer = TicTacToeGameSerializer(game,many=False)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def update_tic_tac_toe(request,pk):
-    game = TicTacToeGame.objects.get(id=pk)
-    try:
-        library_of_board = request.data['library_of_board']
-        game.library_of_board = library_of_board
-        game.legal_moves.remove(request.data['spot'])
-    except:
-        try:
-            game.canceled = request.data['canceled']
-        except:
-            try:
-                game.accepted = request.data['accepted']
-            except:
-                try:
-                    game.rejected = request.data['rejected']
-                except:
-                    pass
-    game.save()
-    serializer = TicTacToeGameSerializer(game,many=False)
-
-    return Response(serializer.data)
 
 @api_view(['POST'])
 def user_joined(request):
